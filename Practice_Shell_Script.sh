a3b7c40b56bf3efbe15c223453a8f98243840d7c
Sai Deepak Reddy <76720108+deepak-vulchi@users.noreply.github.com>
2024-02-09 15:14:12 +0530

    Update and rename Practice_shell.sh to Practice_Shell_Script.sh

--- Practice_Shell_Script.sh
+++ Practice_Shell_Script.sh
@@ -2,15 +2,15 @@
 
 ##################
 # Author: Sai Deepak
-# Version :v1
+# Version:v1
 # Dated: 04/02/2024
-# About: This file is crated for the learning about shell scripting and different commands.
+# About:-  This file is created for learning about shell scripting and different commands.
 # ########################
 
 echo "Hello My name is Sai Deepak Reddy"
 echo "I am Learning about shell Scripting"
 
-# Creating the Varible and accessing it.
+# Creating the variable and accessing it.
 
 firstname=Sai_Deepak
 lastname=Vulchi
