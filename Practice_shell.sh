4c98117ad173aa61eb167a6753b88d48765f7ec0
Sai Deepak Reddy <76720108+deepak-vulchi@users.noreply.github.com>
2024-02-09 15:11:23 +0530

    Add files via upload
    This is a practice file for the shell scripting

---
+++ Practice_shell.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+##################
+# Author: Sai Deepak
+# Version :v1
+# Dated: 04/02/2024
+# About: This file is crated for the learning about shell scripting and different commands.
+# ########################
+
+echo "Hello My name is Sai Deepak Reddy"
+echo "I am Learning about shell Scripting"
+
+# Creating the Varible and accessing it.
+
+firstname=Sai_Deepak
+lastname=Vulchi
+echo  $firstname  $lastname
+
+
+greeting=Hello
+name=$lastname
+echo $greeting $name
+
